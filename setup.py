c67cf6ed7428f7bad53f6fedf5283180cdd8cbad
Vladimir_Sh <user@example.com>
2019-09-13 07:05:46 -0400

    added long description in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
 
 
-setup(
-    name='mocker_client',
-    version='0.0.1dev1',
-    description='client for http mock server https://github.com/vilus/mocker',
-    packages=['mocker_client'],
-    author='Shevchenko Vladimir',
-    url='https://github.com/vilus/mocker_client',
-    install_requires=['requests']
-)
+if __name__ == '__main__':
+
+    with open('README.rst', 'r') as f:
+        long_description = f.read()
+
+    setup(
+        name='mocker_client',
+        version='0.0.2dev1',
+        description='client for http mock server https://github.com/vilus/mocker',
+        long_description=long_description,
+        packages=['mocker_client'],
+        author='Shevchenko Vladimir',
+        url='https://github.com/vilus/mocker_client',
+        install_requires=['requests']
+    )
